a97a4a704301a16d9ddc5c0fe330a3745f8b1550
Phong <minhphong119@gmail.com>
2025-04-03 21:49:42 +0700

    add Beverages.java

---
+++ src/main/java/Model/Beverages.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author daomi
+ */
+public class Beverages extends BaseModel{
+    private int id;
+    private String name;
+    private int price;
+    private int baveragesCategooryId;
+
+    public Beverages() {
+    }
+
+    public Beverages(int id, String name, int price, int baveragesCategooryId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.baveragesCategooryId = baveragesCategooryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getBaveragesCategooryId() {
+        return baveragesCategooryId;
+    }
+
+    public void setBaveragesCategooryId(int baveragesCategooryId) {
+        this.baveragesCategooryId = baveragesCategooryId;
+    }
+    
+    
+}
+
+	
