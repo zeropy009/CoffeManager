f5638df9a0fa10aac6d80bcf574cd8c8bfce5798
zero <zeropy009@gmail.com>
2025-05-10 22:16:45 +0700

    add

---
+++ src/main/java/DAO/StoredProcedureCaller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author zero
+ */
+public interface StoredProcedureCaller {
+    List<Map<String, Object>> callStoredProcedure(String procedureName, List<Object> params);
+}
